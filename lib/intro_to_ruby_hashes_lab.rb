2f542d826102ca9d333ca95db0e0a73d6aef26e1
Jina Strickland <jina30@gmail.com>
2020-06-30 19:06:35 +0000

    Done.

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -10,8 +10,16 @@ end
 
 def update_counting_hash(hash, key)
   if hash[key]
-    hash[key = hash[key] + 1
+    hash[key] += 1
   else
     hash[key] = 1
   end
+  hash
 end
+
+=begin
+def update_counting_hash(hash, key)
+  hash[key] ? hash[key] += 1 : hash[key] = 1
+  hash
+end
+=end
